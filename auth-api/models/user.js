091d0450ebe349a98f58527a7288013d4d2c01b6
mahmoudshaltoot <mahmoudshaltoot@yahoo.com>
2020-03-30 11:19:21 -0700

    joi validation on user model

--- auth-api/models/user.js
+++ auth-api/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const userSchema = mongoose.Schema({
     name: {
@@ -23,4 +24,15 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
+function validateUser (user) {
+    const schema = {
+        name: Joi.string().min(3).max(255).require(),
+        username: Joi.email().min(5).max(255).require(),
+        password: Joi.string().min(5).max(255).require()
+    }
+
+    return Joi.validate(user, schema);
+} 
+
 exports.User = User;
+exports.validate = validateUser;

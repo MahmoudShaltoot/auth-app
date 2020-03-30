bec2fcb230d0224131e5e9d4e1716b9df42e30ef
mahmoudshaltoot <mahmoudshaltoot@yahoo.com>
2020-03-30 15:10:58 -0700

    register new user

--- auth-api/routes/users.js
+++ auth-api/routes/users.js
@@ -1,8 +1,16 @@
+const {User, validate} = require("../models/user");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("users");
+router.post("/", async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user = await user.save();
+    res.send(user);
 })
 
 module.exports = router;

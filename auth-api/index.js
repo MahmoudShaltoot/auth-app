d8e1c011e4e3292de9bfe1554aa5dff1e2318b89
mahmoudshaltoot <mahmoudshaltoot@yahoo.com>
2020-03-30 15:06:20 -0700

    connect to mongodb

--- auth-api/index.js
+++ auth-api/index.js
@@ -1,6 +1,11 @@
+const mongoose = require("mongoose");
 const users = require("./routes/users");
 const express = require("express");
 const app = express();
+
+mongoose.connect('mongodb://localhost/auth-db')
+.then(() => console.log("Connected to MongoDb..."))
+.catch(err => console.log("couldn't connect to MongoDb..."))
 
 app.use(express.json());
 app.use('/api/users', users);

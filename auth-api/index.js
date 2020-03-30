e14afdd9e63c8e2a1679554369941491a5d818a8
mahmoudshaltoot <mahmoudshaltoot@yahoo.com>
2020-03-30 14:49:13 -0700

    add users route

--- auth-api/index.js
+++ auth-api/index.js
@@ -1,8 +1,8 @@
+const users = require("./routes/users");
 const express = require("express");
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('hello world')
-})
+app.use(express.json());
+app.use('/api/users', users);
 
 app.listen(3900, console.log("connected to server on port 3900"));

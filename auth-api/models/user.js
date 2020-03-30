6ea211cb90ef1f8da0b23c7f197e41a640b2ef34
mahmoudshaltoot <mahmoudshaltoot@yahoo.com>
2020-03-30 15:20:42 -0700

    fix validation function

--- auth-api/models/user.js
+++ auth-api/models/user.js
@@ -26,9 +26,9 @@ const User = mongoose.model("User", userSchema);
 
 function validateUser (user) {
     const schema = {
-        name: Joi.string().min(3).max(255).require(),
-        username: Joi.email().min(5).max(255).require(),
-        password: Joi.string().min(5).max(255).require()
+        name: Joi.string().min(3).max(255).required(),
+        email: Joi.string().email().min(5).max(255).required(),
+        password: Joi.string().min(5).max(255).required()
     }
 
     return Joi.validate(user, schema);

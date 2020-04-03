2f9314e4012a4106120ceb687d9c3972b76690ce
mahmoudshaltoot <mahmoudshaltoot@yahoo.com>
2020-04-02 17:53:51 -0700

    add friendly message to validation.js

--- auth-api/startup/validate.js
+++ auth-api/startup/validate.js
@@ -1,8 +1,9 @@
 const config = require("config");
 
+// run in terminal window "set app_jwtPrivateKey=secret-jwt"
 module.exports = () => {
-    if(!config.get('jwtPrivateKey')) {
-        console.log("ERROR : jwtPrivateKey is not defined.");
-        process.exit(1);
-    }    
-}
+  if (!config.get("jwtPrivateKey")) {
+    console.log("ERROR : jwtPrivateKey is not defined.");
+    process.exit(1);
+  }
+};

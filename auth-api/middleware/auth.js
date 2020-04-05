e53edfa1ce4d95469f8c98c7a4c1c0fa45fe49af
mahmoudshaltoot <mahmoudshaltoot@yahoo.com>
2020-04-05 03:22:49 +0200

    Remove 'bearer' from JsonWebToken String

--- auth-api/middleware/auth.js
+++ auth-api/middleware/auth.js
@@ -2,9 +2,12 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports =  (req, res, next) => {
-    const token = req.header('x-auth-token');
+    let token = req.header('Authorization');
+    token = token.split(" ")[1]; 
     if(!token) return res.status(401).send('Access denied. no token provided');
 
+    console.log(token);
+    
     try {   
         const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
         req.user = decoded;
